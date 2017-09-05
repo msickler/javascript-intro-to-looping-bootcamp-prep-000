24f080166fd7907e825c4c6a087815082d1e7d4a
Mariana Sickler <marianaleal_93@hotmail.com>
2017-09-05 14:43:50 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -23,8 +23,7 @@ function doWhileLoop(array) {
   function maybeTrue() {
   return Math.random() >= 0.5
 }
-do {array.pop;
-}
+do {array.pop();}
 while (array.lenght > 0 && maybeTrue());
 return array
 }
